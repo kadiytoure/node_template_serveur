dbc4c63eeb91249f427c45bfb089fcd0895ea247
toure <tourekadiy@gmail.com>
2017-08-23 11:57:06 +0200

    mustache {{}};

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mustache = require('mustache');
 
 let app = express();
+
 app.use(express.static("public"));
+
+app.get("/test", function(req, resp) {
+    let str = mustache.render("Hello {{name}}!!! You are beautiful!", {
+        name: "panda"
+    })
+    resp.send(str)
+});
 app.listen(80, "localhost", function() {
     console.log('Server listening on port 80');
 });

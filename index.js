e3ac5156d1652b0c163a175c84683fb1cd7873d7
toure <tourekadiy@gmail.com>
2017-08-23 11:40:03 +0200

    server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 let app = express();
+app.use(express.static("public"));
 app.listen(80, "localhost", function() {
     console.log('Server listening on port 80');
 });

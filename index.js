c35bcd0e248ba502c21274e195b4c6bc4474156d
toure <tourekadiy@gmail.com>
2017-08-23 12:36:51 +0200

    new file, templates;

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
 const express = require('express');
 const mustache = require('mustache');
 
 let app = express();
 
-app.use(express.static("public"));
+app.get("/", function(req, resp) {
+    res.render('index', {
+        name: 'panda',
+        adjective: 'funny'
+    });
+});
 
 app.get("/test", function(req, resp) {
     let str = mustache.render("Hello {{name}}!!! You are beautiful!", {
@@ -11,6 +17,24 @@ app.get("/test", function(req, resp) {
     })
     resp.send(str)
 });
+
+app.engine("html", function(path, options, callback) {
+    fs.readFile(path, function(err, content) {
+        if (err) {
+            console.error("fail to open template:", err);
+            return callback(err);
+        }
+        let str = mustache.render(content.toString(), options);
+        return callback(null, str);
+    })
+
+});
+// specify the views directory
+app.set('views', './template');
+// register the template engin
+app.set('view engine', 'html');
+app.use(express.static("public"));
+//remplacer par app.engine, app.set
 app.listen(80, "localhost", function() {
     console.log('Server listening on port 80');
 });
